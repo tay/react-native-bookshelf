c8aa8ff7012ea334fcdecbba6fd8d5e0e5004d69
Fiona Tay <me@fionatay.com>
2024-05-13 02:31:15 +0800

    Select formats using memoizable selectBooks

--- src/slices.ts
+++ src/slices.ts
@@ -1,28 +1,30 @@
+import {createSelector} from '@reduxjs/toolkit';
+
 const selectIsLoading = (state: State) => {
   return state.isLoading;
-};
-
-const selectBookById = (state: State, id: number) => {
-  return state.books.find(book => book.id === id);
 };
 
 const selectBooks = (state: State) => {
   return state.books;
 };
 
+const selectBookById = (state: State, id: number) => {
+  return state.books.find(book => book.id === id);
+};
+
 const selectBooksByFormat = (state: State, format: string) => {
   return selectBooks(state).filter(book => book.format === format);
 };
 
-const selectFormats = (state: State) => {
-  return state.books.reduce((acc: Array<string>, book) => {
+const selectFormats = createSelector(selectBooks, books => {
+  return books.reduce((acc: Array<string>, book) => {
     const format = book.format;
     if (!acc.includes(format)) {
       acc.push(format);
     }
     return acc;
   }, []);
-};
+});
 
 export {
   selectBooks,
